fb13ca2179a58ff4390d741fd9db10780876b7db
faaiz251 <mohdfaaizazeem@gmail.com>
2024-12-22 14:03:35 +0530

    commit

--- src/components/landing/Signup/Signup.jsx
+++ src/components/landing/Signup/Signup.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-
 export function Signup() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
